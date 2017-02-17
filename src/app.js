520ffd8e36209ad6153f42e20a81c8a33aac5727
Jordan Mato <jordo754@gmail.com>
2017-02-17 15:50:09 -0500

    finished inclass

--- src/app.js
+++ src/app.js
@@ -5,8 +5,44 @@ const fs = require('fs');
 
 const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const handler = (req, res) => {
+const square = {
+  lastUpdate: new Date().getDate(),
+  x: 0,
+  y: 0,
+  width: 100,
+  height:100
+}
 
+const handler = (req, res) => {
+  fs.readFile(`${__dirname}/../client/index.html`, (err, data) => {
+    if(err){
+      throw err;
+    }
+    
+    res.writeHead(200);
+    res.end(data);
+  });
 };
 
 const app = http.createServer(handler);
+const io = socketio(app);
+
+app.listen(PORT);
+
+io.on('connect', (socket) => {
+  socket.join('lobby');
+  
+  socket.on('movementUpdate', (data) => {
+    square.x += data.xUpdate;
+    square.y += data.yUpdate;
+    square.lastUpdate = new Date().getDate();
+    
+    io.sockets.in('lobby').emit('updatedMovement', square);
+  });
+  
+  socket.on('disconnect', (data) => {
+    socket.leave('lobby');
+  });
+});
+
+console.log(`listening on ${PORT}`);
